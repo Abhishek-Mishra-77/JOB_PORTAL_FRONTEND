5b4e714a304c3814f0d10e4e31a943a8ff5cba65
unknown <abhishekmishra992016@gmail.com>
2024-11-20 15:04:52 +0530

    working on landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,14 +1,28 @@
 
 const LandingPage = () => {
     return (
-        <main>
-            <section>
-                <h1>Find You Dream Job</h1>
+        <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+            <section className="text-center">
+                <h1
+                    className="flex flex-col items-center justify-center">
+                    Find You Dream Job
+                    <span>and get
+                        <img
+                            src="../../public/logo.png"
+                            alt="hiredlogo"
+                            className="h-14 sm:h-24 lg:h-32" />
+                    </span></h1>
             </section>
             <div>
                 {/* button */}
                 {/* carousel */}
             </div>
+            {/* Banner  */}
+            <section>
+                {/* cards  */}
+            </section>
+
+            {/* Accordian */}
         </main>
     )
 }
