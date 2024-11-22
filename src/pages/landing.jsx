648be5ccff384cf5015e13ff4b80646780987190
unknown <abhishekmishra992016@gmail.com>
2024-11-22 12:17:37 +0530

    landing page done

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,21 +1,31 @@
-
+import { Button } from "../components/ui/button";
+import { Link } from "react-router-dom";
 const LandingPage = () => {
     return (
         <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
             <section className="text-center">
                 <h1
-                    className="flex flex-col items-center justify-center">
+                    className="flex flex-col items-center justify-center gradiet-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
                     Find You Dream Job
-                    <span>and get
+                    <span className="flex items-center gap-2 sm:gap-6">and get
                         <img
                             src="../../public/logo.png"
                             alt="hiredlogo"
                             className="h-14 sm:h-24 lg:h-32" />
-                    </span></h1>
+                    </span>
+                </h1>
+                <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">Explore thousands of job listings or find the perfect condidate</p>
             </section>
-            <div>
+            <div className="flex gap-6 justify-center">
                 {/* button */}
+                <Link to="/jobs">
+                    <Button variant="blue" size="xl">Find Jobs</Button>
+                </Link>
+                <Link to="/jobs">
+                    <Button variant="destructive" size="xl">Post a Job</Button>
+                </Link>
                 {/* carousel */}
+
             </div>
             {/* Banner  */}
             <section>
